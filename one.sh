eda32418dcff9af33851faec72e334faad632c70
mallikdevops <50788171+mallikdevops@users.noreply.github.com>
2020-06-15 15:26:31 +0530

    Update one.sh

--- one.sh
+++ one.sh
@@ -1,9 +1,2 @@
 tis is a  my first git project
-this is open source
-git fetch and merge happend
-learning git branches
-learning git push to remote machine
-tis is from master branch
-this is from task2
-this is from task3
-hiii happy for the succeful resolve
+
